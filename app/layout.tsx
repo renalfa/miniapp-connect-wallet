ec7d208477249fb886ec84f6bbfa2e5a8f46b360
rnlfzi <rfaidzilah@gmail.com>
2025-02-13 13:53:43 +0700

    [CHANGE] change manifest json

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <TonConnectUIProvider manifestUrl="https://gold-main-wolverine-336.mypinata.cloud/files/bafkreihcgsotmrfy5sb7aqvgwedyuqydscrtwo2fetij4enzmw7yw27524?X-Algorithm=PINATA1&X-Date=1733734380&X-Expires=30&X-Method=GET&X-Signature=b72181a1aec244f2974b39538a310515d915f7dab537d11af13f769babad7b41">
+        <TonConnectUIProvider manifestUrl="https://miniapp-connect-wallet.vercel.app/tonconnect-manifest.json">
           {children}
         </TonConnectUIProvider>
       </body>
